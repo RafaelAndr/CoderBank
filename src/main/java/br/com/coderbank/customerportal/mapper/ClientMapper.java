cbc2bdce75d0ae2e096cf2871a0ba329bf07d6a7
Rafael <rn206041@gmail.com>
2026-03-07 16:46:18 -0300

    refactor(client): add mapper class

---
+++ src/main/java/br/com/coderbank/customerportal/mapper/ClientMapper.java
@@ -0,0 +1,14 @@
+package br.com.coderbank.customerportal.mapper;
+
+import br.com.coderbank.customerportal.dto.request.ClientRequestDto;
+import br.com.coderbank.customerportal.dto.response.ClientResponseDto;
+import br.com.coderbank.customerportal.entity.Client;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ClientMapper {
+
+    Client toEntity(ClientRequestDto dto);
+
+    ClientResponseDto toDto(Client client);
+}
--- src/main/java/br/com/coderbank/customerportal/service/ClientService.java
+++ src/main/java/br/com/coderbank/customerportal/service/ClientService.java
@@ -5,6 +5,8 @@ import br.com.coderbank.customerportal.dto.response.ClientResponseDto;
 import br.com.coderbank.customerportal.entity.Client;
 import br.com.coderbank.customerportal.enuns.Status;
 import br.com.coderbank.customerportal.exception.ClientAlreadyExistsException;
+import br.com.coderbank.customerportal.exception.DuplicatedEmailException;
+import br.com.coderbank.customerportal.mapper.ClientMapper;
 import br.com.coderbank.customerportal.repository.ClientRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,30 +18,26 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    public ClientResponseDto save(final ClientRequestDto clientRequestDto){
-        checkDuplicatedCpf(clientRequestDto);
-        var clientEntity = new Client();
+    @Autowired
+    private ClientMapper mapper;
 
-        BeanUtils.copyProperties(clientRequestDto, clientEntity);
+    public ClientResponseDto save(final ClientRequestDto clientRequestDto){
+
+        checkDuplicatedCpf(clientRequestDto.cpf());
+
+        var clientEntity = mapper.toEntity(clientRequestDto);
 
         clientEntity.setStatus(Status.ACTIVE);
+
         repository.save(clientEntity);
 
-        return new ClientResponseDto(
-                clientEntity.getId(),
-                clientEntity.getStatus(),
-                clientEntity.getCreatedByUser(),
-                clientEntity.getCreatedDateAndTime(),
-                clientEntity.getEditedByUser(),
-                clientEntity.getEditedDateAndTime()
-        );
+        return mapper.toDto(clientEntity);
     }
 
-    private void checkDuplicatedCpf(final ClientRequestDto clientRequestDto){
-        var clientCpf = clientRequestDto.cpf();
+    private void checkDuplicatedCpf(String cpf){
 
-        if (repository.existsByCpf(clientCpf)) {
-            throw new ClientAlreadyExistsException("Client with cpf " + clientCpf + " already exists");
+        if (repository.existsByCpf(cpf)) {
+            throw new ClientAlreadyExistsException("Client with cpf " + cpf + " already exists");
         }
     }
 }
