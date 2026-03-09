b667f899cb6e2e7c7bbafd1a800c084acc95db88
Rafael <rn206041@gmail.com>
2026-03-09 14:37:25 -0300

    feat(client): add globalHandleValidation with ProblemDetail return

--- src/main/java/br/com/coderbank/customerportal/dto/request/ClientRequestDto.java
+++ src/main/java/br/com/coderbank/customerportal/dto/request/ClientRequestDto.java
@@ -1,15 +1,21 @@
 package br.com.coderbank.customerportal.dto.request;
 
 import jakarta.transaction.Status;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record ClientRequestDto(
-        @NotBlank(message = "Campo obrigatório")
+        @NotBlank(message = "required field")
         String name,
-        @NotBlank(message = "Campo obrigatório")
+        @NotBlank(message = "required field")
         String cpf,
+        @Email(message = "The field email must be valid")
         String email,
         String addres,
+        @Min(value = 18, message = "The minimum age is 18")
+        @Max(value = 80, message = "The maximum age is 80")
         Integer age
 ) {
 }
--- src/main/java/br/com/coderbank/customerportal/exception/ControllerHandlerException.java
+++ src/main/java/br/com/coderbank/customerportal/exception/ControllerHandlerException.java
@@ -1,26 +1,103 @@
 package br.com.coderbank.customerportal.exception;
 
 import br.com.coderbank.customerportal.dto.response.ErrorResponseDto;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ControllerHandlerException {
+
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ProblemDetail handleMethodArgumentNotValidExcepition(MethodArgumentNotValidException e){
+//
+//        final var validationErrors = buildHashMapValidationErrors(e);
+//
+//        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, validationErrors.toString());
+//
+//        problemDetail.setTitle("Data sent in the request is invalid");
+//        problemDetail.setType(URI.create("http://www.siteexemplo.com.br"));
+//        problemDetail.setProperty("errors", validationErrors);
+//
+//        return problemDetail;
+//    }
+//
+//    private static HashMap<Object, Object> buildHashMapValidationErrors(MethodArgumentNotValidException e) {
+//        final var errors = new HashMap<>();
+//
+//        e.getBindingResult()
+//                .getAllErrors()
+//                .forEach((error) -> {
+//
+//                    var fieldName = ((FieldError) error).getField();
+//
+//                    var errorMessage = error.getDefaultMessage();
+//
+//                    errors.put(fieldName, errorMessage);
+//                });
+//        return errors;
+//    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleValidation(MethodArgumentNotValidException e){
+
+        Map<String, String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        FieldError::getDefaultMessage
+                ));
+
+        var problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                "Validation failed"
+        );
+
+        problemDetail.setTitle("Invalid request data");
+        problemDetail.setProperty("errors", errors);
+
+        return problemDetail;
+    }
 
     @ExceptionHandler({ClientAlreadyExistsException.class})
     @ResponseBody
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponseDto handleClientAlreadyExists(ClientAlreadyExistsException e){
-        return new ErrorResponseDto(e.getMessage());
+    public ProblemDetail handleClientAlreadyExists(ClientAlreadyExistsException e){
+
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+
+        problemDetail.setTitle("Conflict: Existing cpf");
+        problemDetail.setType(URI.create("http://www.siteexemplo.com.br"));
+
+        return problemDetail;
     }
 
     @ExceptionHandler({DuplicatedEmailException.class})
     @ResponseBody
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponseDto handleDuplicatedEmailException(DuplicatedEmailException e){
-        return new ErrorResponseDto(e.getMessage());
+    public ProblemDetail handleDuplicatedEmailException(DuplicatedEmailException e){
+
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+
+        problemDetail.setTitle("Conflict: Existing email");
+        problemDetail.setType(URI.create("http://www.siteexemplo.com.br"));
+
+        return problemDetail;
     }
+
 }
